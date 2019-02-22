ce560408324fb6c334efe1df0c538018db1e4c26
Viktor Rosvall <virosen@yahoo.se>
2019-02-22 16:46:55 +0100

    Index ready for placeholder images

--- includes/head-tag-contents.php
+++ includes/head-tag-contents.php
@@ -5,7 +5,11 @@
     <meta name="keywords" content="" />
 <?php } ?>
 
-<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="/stylesheets/style.css">
 <style>
     #main-content {
--- index.php
+++ index.php
@@ -1,22 +1,48 @@
 <?php include("includes/a_config.php"); ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-    <?php include("includes/head-tag-contents.php"); ?>
+	<?php include("includes/head-tag-contents.php"); ?>
 </head>
+
 <body>
 
-<?php include("includes/design-top.php"); ?>
-<?php include("includes/navigation.php"); ?>
+	<?php include("includes/design-top.php"); ?>
+	<?php include("includes/navigation.php"); ?>
 
-<div class="container" id="main-content">
-    <h1>Welcome to my website!</h1>
-    <h5>The header, navigation and footer is dynamically created using PHP!</h5>
-    <p>Currently experimenting with cURL scraping...</p>
-    <br>
-</div>
+	<div class="container" id="main-content">
+		<h2>Stack</h2>
+		<p>En stack fungerar engligt LIFO (Last in First out) principen. 
+            Det betyder att det senaste elementet som läggs
+			på stacken är det första som tas bort.</p>
+		<ul class="list-unstyled">
+			<li class="media">
+				<img src="images/stackpush.gif" class="mr-3" alt="Stack pop">
+				<div class="media-body">
+					<h5 class="mt-0 mb-1">Push</h5>
+					<p>Push operationen lägger ett element först i stacken (längst upp)</p>
+				</div>
+			</li>
+			<li class="media my-4">
+				<img src="images/stackpop.gif" class="mr-3" alt="...">
+				<div class="media-body">
+					<h5 class="mt-0 mb-1">Pop</h5>
+					<p>Pop operationen tar bort det översta elementet ur stacken</p>
+				</div>
+			</li>
+			<li class="media">
+				<img src="..." class="mr-3" alt="...">
+				<div class="media-body">
+					<h5 class="mt-0 mb-1">Peek</h5>
+					<p>Tittar på det översta elementet i stacken</p>
+				</div>
+			</li>
+		</ul>
+	</div>
 
-<?php include("includes/footer.php"); ?>
+	<?php include("includes/footer.php"); ?>
 
 </body>
+
 </html>
